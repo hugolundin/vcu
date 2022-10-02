d3c9cbb6169d956e02ad087a2fa8421f2c144751
Hugo Lundin <hugo@lundin.dev>
2022-10-02 19:50:03 +0200

    Fix include

--- src/zephyr/main.c
+++ src/zephyr/main.c
@@ -4,7 +4,7 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
-#include "../vcu/state_machine.h"
+#include "vcu/state_machine.h"
 
 int main(int argc, char const *argv[])
 {
