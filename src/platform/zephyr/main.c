a2229948ef456cffacdaa542965eed5c6a58f4ca
Hugo Lundin <hugo@lundin.dev>
2022-10-08 23:13:26 +0200

    Fix can bus

--- src/platform/zephyr/main.c
+++ src/platform/zephyr/main.c
@@ -1,12 +1,19 @@
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/can.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
-int main(int argc, char const *argv[])
+const struct device * can1 = DEVICE_DT_GET(DT_NODELABEL(can1));
+
+int main(void)
 {
-	const struct device *sensor;
-    
+    if (!device_is_ready(can1)) {
+        return;
+    }
+
+    can_start(can1);
+
     return 0;
 }
