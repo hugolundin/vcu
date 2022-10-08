4b413707b8373c78423d8d3e3c3e30d3e4696861
Hugo Lundin <hugo@lundin.dev>
2022-10-08 23:37:46 +0200

    Add tasks

--- src/platform/zephyr/main.c
+++ src/platform/zephyr/main.c
@@ -5,15 +5,45 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
+CAN_MSGQ_DEFINE(can_rx_queue, 2);
 const struct device * can1 = DEVICE_DT_GET(DT_NODELABEL(can1));
+
+static void apps_task(void *arg1, void *arg2, void *arg3)
+{
+
+}
+
+static void brake_task(void *arg1, void *arg2, void *arg3)
+{
+
+}
+
+static void can_tx_task(void *arg1, void *arg2, void *arg3)
+{
+
+}
+
+static void cooling_task(void *arg1, void *arg2, void *arg3)
+{
+
+}
+
+static void inverter_task(void *arg1, void *arg2, void *arg3)
+{
+
+}
+
+static void speaker_task(void *arg1, void *arg2, void *arg3)
+{
+
+}
 
 int main(void)
 {
-    if (!device_is_ready(can1)) {
-        return;
-    }
-
     can_start(can1);
+    struct can_frame frame;
 
-    return 0;
+    while (1) {
+        k_msgq_get(&can_rx_queue, &frame, K_FOREVER);
+    }
 }
